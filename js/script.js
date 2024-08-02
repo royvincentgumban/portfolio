7ad0e5a05cf6e7884ad7da014f9640a0344f3d03
Roy <gumbanroyst@gmail.com>
2024-08-02 22:11:12 +0800

    add a loader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+window.addEventListener('load', () => {
+
+  const startTime = performance.now();
+
+  const removeLoader = () => {
+    const endTime = performance.now();
+    const loadDuration = endTime - startTime;
+    const minLoaderDuration = 3000; 
+
+
+    const duration = Math.max(minLoaderDuration, loadDuration);
+
+    setTimeout(() => {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('overflow-hidden');
+    }, duration);
+  };
+
+
+  document.body.classList.add('overflow-hidden');
+  
+  removeLoader();
+});
+
 // display menu
 const toggleShow = document.getElementById("showToggle");
 const toggleClose = document.getElementById("closeToggle");
